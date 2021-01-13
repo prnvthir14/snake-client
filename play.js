a5af24c053e5560052a160e36db2ee2bcd39e689
pranav <prnvthir@gmail.com>
2021-01-13 12:31:35 -0500

    updated connection and handling for idle player

---
+++ play.js
@@ -0,0 +1,32 @@
+//client 
+
+const net = require('net');
+
+//establish connection with host server
+const connect = function () {
+
+  const conn = net.createConnection({
+
+    host:'135.23.222.131',
+    port: 50542
+  
+  });
+
+  conn.setEncoding('utf8');
+
+  return conn;
+  
+  //event handler to handle incomming data 
+  client.on('data', () => {
+
+    // client.write(`${data}`)
+    console.log(`${data}`)
+
+  })
+
+}
+
+console.log('Connecting ...');
+connect();
+
+
