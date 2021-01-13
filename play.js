2ebbc674bf014760758a0889cf8867085234f545
pranav <prnvthir@gmail.com>
2021-01-13 12:40:59 -0500

    working file

--- play.js
+++ play.js
@@ -1,32 +1,10 @@
+
 //client 
 
-const net = require('net');
+//const net = require('net');
+const connect = require('./client')
 
-//establish connection with host server
-const connect = function () {
-
-  const conn = net.createConnection({
-
-    host:'135.23.222.131',
-    port: 50542
-  
-  });
-
-  conn.setEncoding('utf8');
-
-  return conn;
-  
-  //event handler to handle incomming data 
-  client.on('data', () => {
-
-    // client.write(`${data}`)
-    console.log(`${data}`)
-
-  })
-
-}
 
 console.log('Connecting ...');
 connect();
-
 
